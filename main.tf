052c700fa5af7f215424395eaffa0c7ceee8a923
kief <kmorris@thoughtworks.com>
2016-12-16 11:53:09 +0000

    Added EIP to the instance, still not connecting out

--- main.tf
+++ main.tf
@@ -15,6 +15,15 @@ resource "aws_key_pair" "auth" {
   public_key = "${file(var.public_key_path)}"
 }
 
+resource "aws_eip" "gocd_eip" {
+  vpc = true
+}
+
+resource "aws_eip_association" "gocd_eip" {
+  instance_id = "${aws_instance.gocd.id}"
+  allocation_id = "${aws_eip.gocd_eip.id}"
+}
+
 resource "aws_instance" "gocd" {
   tags {
     Name = "GoCD Server"
@@ -25,9 +34,17 @@ resource "aws_instance" "gocd" {
   vpc_security_group_ids = ["${module.vpc.ssh_securitygroup_id}"]
   subnet_id = "${module.vpc.main_subnet_id}"
   key_name = "${aws_key_pair.auth.id}"
-  associate_public_ip_address = true
+  # associate_public_ip_address = true
 }
 
+# echo "deb https://download.go.cd /" | sudo tee /etc/apt/sources.list.d/gocd.list
+# curl https://download.go.cd/GOCD-GPG-KEY.asc | sudo apt-key add -
+# sudo apt-get update
+# sudo apt-get install go-server
+# sudo /etc/init.d/go-server [start|stop|status|restart]
+
+
+
 output "gocd_server_ip" {
-  value = "${aws_instance.gocd.public_ip}"
+  value = "${aws_eip.gocd_eip.public_ip}"
 }
