25018d8a6c8516dd913fcd8816f9076320aac693
kief <kmorris@thoughtworks.com>
2017-01-05 15:17:11 +0000

    Install the yaml-config plugin on the gocd server.

--- provisioning-scripts/gocd_server.sh
+++ provisioning-scripts/gocd_server.sh
@@ -2,5 +2,24 @@
 
 echo "deb https://download.gocd.io /" | sudo tee /etc/apt/sources.list.d/gocd.list
 curl https://download.gocd.io/GOCD-GPG-KEY.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install go-server -y
+apt-get update
+apt-get install go-server -y
+service go-server stop
+
+cat > /etc/go/cruise-config.xml <<ENDCONFIG
+  <server artifactsdir="artifacts" agentAutoRegisterKey="3b734c17-3361-4718-a889-40b255729ccc" commandRepositoryLocation="default" serverId="" />
+  <config-repos>
+    <config-repo plugin="yaml.config.plugin">
+      <git url="https://github.com/kief/spin-vpc.git" />
+    </config-repo>
+  </config-repos>
+ENDCONFIG
+chown go:go /etc/go/cruise-config.xml
+
+( 
+  cd /var/lib/go-server/plugins/external
+  curl -sLO https://github.com/tomzo/gocd-yaml-config-plugin/releases/download/0.4.0/yaml-config-plugin-0.4.0.jar
+)
+chown go:go /var/lib/go-server/plugins/external/yaml-config-plugin-*.jar
+
+service go-server start
